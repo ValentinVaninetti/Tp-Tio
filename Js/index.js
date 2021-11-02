cd9511744899451e1be7fec451c6827e49119e7c
Valentin Vaninetti <valenga20@gmail.com>
2021-11-02 15:38:06 -0300

    Edité index.js

--- Js/index.js
+++ Js/index.js
@@ -1,2 +1,3 @@
-console.log("Che Vaca... hacemos un chat por aca?")
-console.log("Dale, que contas de tu vida?")
+console.log("Che Vaca... hacemos un chat por aca?");
+console.log("Dale, que contas de tu vida?");
+console.log("Como venis con TIO?");
