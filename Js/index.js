2198fb7d08ae37470e6b839596cf4f8cdee3b232
Valentin Vaninetti <valenga20@gmail.com>
2021-11-08 15:53:47 -0300

    modifique index.js

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,6 @@
 console.log("Che Vaca... hacemos un chat por aca?")
 console.log("Dale, que contas de tu vida?")
 console.log("Tengo un noticion para la proxima sesion de mates")
+"use strict"
+
+console.log ("Hello World!")
