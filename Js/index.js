d35e60b6d99853df2bb76d8f5c2fb0469db4e8e0
Valentin Vaninetti <valenga20@gmail.com>
2021-11-08 16:42:30 -0300

    elimine comentarios

--- Js/index.js
+++ Js/index.js
@@ -4,15 +4,12 @@ document.addEventListener("DOMContentLoaded", iniciarPagina)
     function iniciarPagina() {
     console.log ("Hello World!")
 
-    let button = document.getElementById("buttonShow");
+    let button = document.querySelector("buttonShow");
     button.addEventListener("click", saludar);
 
     function saludar() {
     console.log("Hola, soy una funcion")
-    }
-
-    //agregar en css una clase para que se muestre el nav
-    //agregar en css una clase para que se oculte el nav que este todo el tiempo Hidden.
+    } 
 
     button.addEventListener("click", mostrarNav);
 
@@ -20,7 +17,5 @@ document.addEventListener("DOMContentLoaded", iniciarPagina)
         let nav = document.getElementById("nav");
         nav.classList.toggle("mostrar");
     }
-
-    
     
 }
