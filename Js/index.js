2e8b9cee8bb7a7189970f577d3bd7393671a32ab
Valentin Vaninetti <valenga20@gmail.com>
2021-11-08 16:39:04 -0300

    modifique Js

--- Js/index.js
+++ Js/index.js
@@ -1,19 +1,26 @@
 "use strict"
+document.addEventListener("DOMContentLoaded", iniciarPagina)
 
-console.log ("Hello World!")
+    function iniciarPagina() {
+    console.log ("Hello World!")
 
-let button = document.getElementById("buttonShow");
-button.addEventListener("click", saludar);
+    let button = document.getElementById("buttonShow");
+    button.addEventListener("click", saludar);
 
-function saludar() {
-  console.log("Hola, soy una funcion")
+    function saludar() {
+    console.log("Hola, soy una funcion")
+    }
+
+    //agregar en css una clase para que se muestre el nav
+    //agregar en css una clase para que se oculte el nav que este todo el tiempo Hidden.
+
+    button.addEventListener("click", mostrarNav);
+
+    function mostrarNav() {
+        let nav = document.getElementById("nav");
+        nav.classList.toggle("mostrar");
+    }
+
+    
+    
 }
-
-let nav = document.getElementById("nav");
-button.addEventListener("click", mostrarNav);
-
-function mostrarNav() {
-  nav.classList.toggle("mostrar");
-}
-//agregar en css una clase para que se muestre el nav
-//agregar en css una clase para que se oculte el nav que este todo el tiempo Hidden.
